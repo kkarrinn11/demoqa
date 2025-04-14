ec967b6d10fa2a07df4217a48fa722ebd0c24cbd
Karina_M <karina_karina2005@icloud.com>
2025-04-15 00:10:25 +0300

    В процессе №8

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -1,25 +1,24 @@
 from pages.demoqa import DemoQa
 from pages.elements_page import ElementsPage
 
-# def test_check_footer(browser):
-#     demo_qa_page = DemoQa(browser)
-#     demo_qa_page.visit()
-#     assert demo_qa_page.text_footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
-#
-# def test_check_text(browser):
-#     demo_qa_page = DemoQa(browser)
-#     el_page = ElementsPage(browser)
-#
-#     demo_qa_page.visit()
-#     demo_qa_page.btn_elements.click()
-#     assert el_page.text.get_text() == 'Please select an item from left to start practice.'
+def test_check_footer(browser):
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    assert demo_qa_page.text_footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+
+def test_check_text(browser):
+    demo_qa_page = DemoQa(browser)
+    el_page = ElementsPage(browser)
+
+    demo_qa_page.visit()
+    demo_qa_page.btn_elements.click()
+    assert el_page.text.get_text() == 'Please select an item from left to start practice.'
 
 
 def test_page_elements (browser):
     el_page = ElementsPage (browser)
 
     el_page.visit()
-    # assert el_page.text_elements.get_text() == 'Please select an item from left to start practice.'
     assert el_page.icon.exist()
     assert el_page.btn_sidebar_first.exist()
     assert el_page.btn_sidebar_first_textbox.exist()
